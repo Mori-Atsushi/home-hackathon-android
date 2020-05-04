7691d4503c60d8f99d0242d7204b0b40d68cf931
Mori Atsushi <at.sushi.tb@gmail.com>
2020-05-04 16:15:42 +0900

    Implement to send events

--- app/src/main/kotlin/com/example/home_hackathon/repository/EventRepository.kt
+++ app/src/main/kotlin/com/example/home_hackathon/repository/EventRepository.kt
@@ -1,15 +1,27 @@
 package com.example.home_hackathon.repository
 
-import android.util.Log
 import com.example.home_hackathon.pb.App
 import com.example.home_hackathon.pb.AppServiceGrpcKt.AppServiceCoroutineStub
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class EventRepository(
     private val api: AppServiceCoroutineStub
 ) {
-    suspend fun ping() {
-        val request = App.PingRequest.newBuilder().build()
-        val response = api.ping(request)
-        Log.d("TAG", response.toString())
+    fun event(input: Flow<Boolean>): Flow<Boolean> {
+        val request = input.map {
+            App.Event.newBuilder()
+                .setIsDown(it)
+                .setSoundId(1)
+                .build()
+        }.map {
+            App.EventRequest.newBuilder()
+                .setEvent(it)
+                .build()
+        }
+        val response = api.event(request)
+        return response.map {
+            it.event.isDown
+        }
     }
 }
--- app/src/main/kotlin/com/example/home_hackathon/ui/MainViewModel.kt
+++ app/src/main/kotlin/com/example/home_hackathon/ui/MainViewModel.kt
@@ -1,18 +1,36 @@
 package com.example.home_hackathon.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.home_hackathon.audio.AudioEngine
 import com.example.home_hackathon.repository.EventRepository
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val repository: EventRepository,
     private val audioEngine: AudioEngine
 ) : ViewModel() {
+    private val channel = Channel<Boolean>(Channel.BUFFERED)
+
+    init {
+        repository.event(channel.consumeAsFlow())
+            .onEach { audioEngine.setToneOn(it) }
+            .launchIn(viewModelScope)
+    }
+
     fun touchDown() {
-        audioEngine.setToneOn(true)
+        viewModelScope.launch {
+            channel.send(true)
+        }
     }
 
     fun touchUp() {
-        audioEngine.setToneOn(false)
+        viewModelScope.launch {
+            channel.send(false)
+        }
     }
 }
