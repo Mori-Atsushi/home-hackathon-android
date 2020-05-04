f49bccab4d587da621c5f09d43fa7cfc9d24ba1f
Mori Atsushi <at.sushi.tb@gmail.com>
2020-05-04 19:37:25 +0900

    Add main layout

--- app/src/main/kotlin/com/example/home_hackathon/ui/MainActivity.kt
+++ app/src/main/kotlin/com/example/home_hackathon/ui/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.home_hackathon.ui
 
+import android.os.Bundle
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.home_hackathon.R
+import com.example.home_hackathon.databinding.ActivityMainBinding
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+    }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action) {
