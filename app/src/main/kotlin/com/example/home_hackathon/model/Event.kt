a0da573ced4e6b9a25df23264e680b5e79dd0765
Mori Atsushi <at.sushi.tb@gmail.com>
2020-05-07 21:05:10 +0900

    Ktlint format

--- app/src/main/kotlin/com/example/home_hackathon/model/Event.kt
+++ app/src/main/kotlin/com/example/home_hackathon/model/Event.kt
@@ -4,9 +4,9 @@ sealed class Event {
     data class SoundEvent(
         val sound: Sound,
         val userID: String
-    ): Event()
+    ) : Event()
 
     data class UserEvent(
         val userIDs: List<String>
-    ): Event()
+    ) : Event()
 }
